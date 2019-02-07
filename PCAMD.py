4063a86503a0afbb9a4c1cfd5a96fd5ae7cb1723
Max Yatsenko <mgmyatsenko@tamu.edu>
2019-02-07 16:36:49 -0600

    Good plot, not turned

--- PCAMD.py
+++ PCAMD.py
@@ -1,23 +1,22 @@
 # Imports
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.io as scio
 
 # Microseismic data input
-MD = np.loadtxt('145.txt')
-MD = MD.reshape(2000, 36)
+mat = scio.loadmat('tensor24.mat')
+MD = mat['xyztensor']
+print(MD.shape)
 
-# Get only x component
-y = 1
-mdx = np.zeros([MD.shape[0], int(MD.shape[1]/3)])
-for i in range(0, int(MD.shape[1]/3)):
-    mdx[:, i] = MD[:, y]
-    y = y+3
+# Separation into x, y, z components
+mdx = MD[:, :, 64]
+mdy = MD[:, :, 65]
+mdz = MD[:, :, 66]
 
 # Plot the data
 k = mdx.shape[1]
-plt.figure(figsize=(10,15))
 for i in range(0, k):
     plt.subplot(k, 1, i+1)
-    plt.plot(mdx[:, i], '.-')
+    fig = plt.plot(mdx[:, i])
     plt.axis('off')
 plt.show()
